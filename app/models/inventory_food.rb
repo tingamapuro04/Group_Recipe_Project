44d0ca9c589bd58b0d127946b84dc1a390d4bc3d
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-20 09:29:25 +0300

    Linters auto fix

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class InventoryFood < ApplicationRecord
   belongs_to :inventory, class_name: 'Inventory'
   belongs_to :food, class_name: 'Food'
--- app/views/inventory_foods/show.json.jbuilder
+++ app/views/inventory_foods/show.json.jbuilder
@@ -1,3 +1,1 @@
-# frozen_string_literal: true
-
 json.partial! 'inventory_foods/inventory_food', inventory_food: @inventory_food
