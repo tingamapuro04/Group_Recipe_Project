1e6dbdb7719020fb735bc09d792ed387023a8272
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-21 06:40:42 +0200

    Add buttons and implement the delete methods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,5 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_food, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index]
 
@@ -51,7 +52,7 @@ class FoodsController < ApplicationController
   # DELETE /foods/1 or /foods/1.json
   def destroy
     @food.destroy
-
+    
     respond_to do |format|
       format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
